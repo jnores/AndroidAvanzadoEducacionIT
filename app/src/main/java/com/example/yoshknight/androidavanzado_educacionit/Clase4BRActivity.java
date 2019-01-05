b84eec6374805d2ae4d4134190286e859768c8d3
yoshknight <yoshi40581>
2019-01-05 10:55:43 -0300

    Clase 4 - BroadcastReceiver
    Agrego activity de ejemplo de broadcastReceiver.

---
+++ app/src/main/java/com/example/yoshknight/androidavanzado_educacionit/Clase4BRActivity.java
@@ -0,0 +1,82 @@
+package com.example.yoshknight.androidavanzado_educacionit;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.yoshknight.androidavanzado_educacionit.services.TemporizadorService;
+
+/**
+ * 1. Defino el ID del EVENTO que escucha
+ * 2. Creo un intentFilter que filtre este ID de EVENTO
+ * 3. Extiendo la clase BroadcastReceiver Para no tener clases anonimas dando vueltas.
+ * 4. Estiando onStart y onStop para registrar  y desregistrar el receiver.
+ *
+ */
+public class Clase4BRActivity extends AppCompatActivity {
+
+    public static final String ACCION = Clase4BRActivity.class.getPackage().getName()+"Temporizador";
+
+    /**
+     * Filtra las acciones que se estan recibiendo
+     */
+    IntentFilter intentFilter;
+    /**
+     * Escuchar eventos. Procesa los eventos que deja pasar el intentFilter.
+     */
+    TemporizadorReceiver receiver;
+
+    Button btnStart;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clase4_br);
+
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnStart.setEnabled(false);
+                Intent intent = new Intent(Clase4BRActivity.this, TemporizadorService.class);
+                startService(intent);
+                Toast.makeText(Clase4BRActivity.this, "Temporizador Iniciado",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        receiver = new TemporizadorReceiver();
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(ACCION);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
+    private class TemporizadorReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            btnStart.setEnabled (true);
+            Toast.makeText(Clase4BRActivity.this, "Temporizador Terminado",Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/yoshknight/androidavanzado_educacionit/services/TemporizadorService.java
@@ -0,0 +1,64 @@
+package com.example.yoshknight.androidavanzado_educacionit.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.example.yoshknight.androidavanzado_educacionit.Clase4BRActivity;
+
+public class TemporizadorService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        new TemporizadorTask(startId).execute();
+
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    private class TemporizadorTask extends AsyncTask<Void,Void, Integer> {
+
+        private int serviceId;
+
+        public TemporizadorTask(int serviceId) {
+            this.serviceId = serviceId;
+        }
+
+        @Override
+        protected Integer doInBackground(Void... voids) {
+
+            long tiempoFinal = System.currentTimeMillis()+5000;
+
+            while( System.currentTimeMillis() < tiempoFinal)
+
+            {
+                try {
+                    Thread.sleep(tiempoFinal-System.currentTimeMillis() );
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+
+            Intent intent = new Intent(Clase4BRActivity.ACCION);
+
+            LocalBroadcastManager.getInstance(TemporizadorService.this)
+                    .sendBroadcast(intent);
+
+            stopSelf(serviceId);
+        }
+    }
+
+}
